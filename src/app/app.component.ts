5b10872cde993a4c00f801332b4fde37643da706
hasanat72 <alihasanat427@yahoo.com>
2022-02-20 12:56:35 +0100

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import {GitHubService} from "./github.service";
+import {filter, debounceTime,distinctUntilChanged} from "rxjs/operators";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <input class="form-control" type="search" [formControl]="searchControl">
+    <div *ngIf="isLoading"><i class="fa-solid fa-spinner fa-spin fa-4x"></i>
+    </div>
+
+    <h3>GitHub User Results</h3>
+    <div *ngIf="isLoading">
+      <i class="fa fa-spinner fa-spin fa-3x"></i>
+
+    </div>
+    <div *ngFor="let user of users" class="media">
+      <div class="media">
+        <a href="{{user['html_url']}}">
+          <img src="{{user['avatar_url']}}" class="mr-4" alt="..." width="64" height="64">
+        </a>
+        <div class="media-body">
+          <h5 class="mt-0">{{user["login"]}}</h5>
+          Score: {{user["score"]}}
+
+        </div>
+
+      </div>
+
+    </div>
+
+  `,
+  providers: [GitHubService]
 })
 export class AppComponent {
-  title = 'API';
+  searchControl = new FormControl();
+  isLoading = true;
+  users = [];
+
+  constructor(private _githubService: GitHubService) {
+
+  }
+
+  ngOnInit(){
+    this.searchControl.valueChanges
+      .pipe(filter(text=>text.length >=3),
+        debounceTime(400),
+        distinctUntilChanged())
+      .subscribe(value =>{
+
+        this._githubService.getGitHubData(value)
+          .subscribe(data =>{
+            this.isLoading = false;
+            // @ts-ignore
+            this.users = data.items;
+            // @ts-ignore
+            console.log(data.items)
+
+      });
+
+      });
+  }
 }
